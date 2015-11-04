1ec655f70c26e323bd4b55d627fb32c431f5f809
Antoine Palazzolo <a.palazzolo@kreactive.com>
2015-11-04 18:22:45 +0100

    move file to framework project

---
+++ FunctionalJSON/FunctionalJSON.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalJSON.h
+//  FunctionalJSON
+//
+//  Created by Antoine Palazzolo on 04/11/15.
+//  Copyright © 2015 Kreactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalJSON.
+FOUNDATION_EXPORT double FunctionalJSONVersionNumber;
+
+//! Project version string for FunctionalJSON.
+FOUNDATION_EXPORT const unsigned char FunctionalJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalJSON/PublicHeader.h>
+
+
