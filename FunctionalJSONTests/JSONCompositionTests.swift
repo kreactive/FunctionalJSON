165fd4578cd5e031b5b356fed25562c9f9f4ba05
Antoine Palazzolo <a.palazzolo@kreactive.com>
2015-11-06 14:49:20 +0100

    add compostion test file

---
+++ FunctionalJSONTests/JSONCompositionTests.swift
@@ -0,0 +1,31 @@
+//
+//  JSONCompositionTests.swift
+//  FunctionalJSON
+//
+//  Created by Antoine Palazzolo on 06/11/15.
+//  Copyright © 2015 Kreactive. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import FunctionalJSON
+import FunctionalBuilder
+
+
+
+class JSONCompositionTests : XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func testBaseTypes() {
+    }
+}
