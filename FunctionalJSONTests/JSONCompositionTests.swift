0b156ff05f23104e62741900962669ea55404b07
Antoine Palazzolo <a.palazzolo@kreactive.com>
2015-11-06 17:35:04 +0100

    add composition tests

--- FunctionalJSONTests/JSONCompositionTests.swift
+++ FunctionalJSONTests/JSONCompositionTests.swift
@@ -75,6 +75,74 @@ class JSONCompositionTests : XCTestCase {
         
         let readOpt2 = try? json["prop7"].validate(JSONPath("p1").read(Int) <&> JSONPath("p2").read(String))
         XCTAssert(readOpt2 == nil)
-
+    }
+    
+    func testCompositionError() {
+        let source = [
+            "prop1" : 3,
+            "prop2" : ["this" : "is","madness" : "!"],
+            "prop3" : "http://google‘ë“{{‘¶.com",
+            "prop4" : ["p1" : NSNull()]
+        ]
+        
+        let json = try! jsonFromAny(source)
+        
+        let subRead = JSONPath("p1").read(String)
+        let finalRead = JSONRead(
+            JSONPath("prop1").read(String) <&>
+            JSONPath(["prop2","madness"]).read(Array.jsonRead(String)) <&>
+            JSONPath("prop3").read(NSURL.jsonRead) <&>
+            JSONPath(["prop3",0,"coucou"]).read(NSURL.jsonRead) <&>
+            JSONPath("prop4").read(subRead) <&>
+            JSONPath("prop4").read(JSONPath("p4").read(String)<&>JSONPath("p5").read(String))
+        )
+        
+        do {
+            try json.validate(finalRead)
+        } catch JSONReadError.CompositionError(let errors) {
+            if case JSONReadError.BadValueType(let path) = errors[0] {
+                XCTAssertEqual(path, JSONPath("prop1"))
+            } else {
+                XCTFail("should be a bad value error")
+            }
+            
+            if case JSONReadError.BadValueType(let path) = errors[1] {
+                XCTAssertEqual(path, JSONPath(["prop2","madness"]))
+            } else {
+                XCTFail("should be a bad value error \(errors[1])")
+            }
+            
+            if case JSONReadError.TransformError(let path, underlying: _) = errors[2] {
+                XCTAssertEqual(path, JSONPath(["prop3"]))
+            } else {
+                XCTFail("should be a TransformError error \(errors[2])")
+            }
+            
+            if case JSONReadError.ValueNotFound(let path) = errors[3] {
+                XCTAssertEqual(path, JSONPath(["prop3",0,"coucou"]))
+            } else {
+                XCTFail("should be a ValueNotFound error \(errors[3])")
+            }
+            
+            if case JSONReadError.ValueNotFound(let path) = errors[4] {
+                XCTAssertEqual(path, JSONPath(["prop4","p1"]))
+            } else {
+                XCTFail("should be a ValueNotFound error \(errors[4])")
+            }
+            
+            if case JSONReadError.ValueNotFound(let path) = errors[5] {
+                XCTAssertEqual(path, JSONPath(["prop4","p4"]))
+            } else {
+                XCTFail("should be a ValueNotFound error \(errors[5])")
+            }
+            if case JSONReadError.ValueNotFound(let path) = errors[6] {
+                XCTAssertEqual(path, JSONPath(["prop4","p5"]))
+            } else {
+                XCTFail("should be a ValueNotFound error \(errors[6])")
+            }
+            
+        } catch {
+            XCTFail("should be a composition error, \(error)")
+        }
     }
 }
